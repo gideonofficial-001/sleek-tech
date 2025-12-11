f975b1928b72228a6318c683e96183310b6f2304
Gideon Sleek <kapyengadeux27@gmail.com>
2025-12-12 00:25:04 +0300

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,17 @@
+const particlesContainer = document.getElementById("particles");
+
+function createParticle() {
+    const particle = document.createElement("div");
+    particle.classList.add("particle");
+
+    particle.style.left = Math.random() * 100 + "vw";
+    particle.style.animationDuration = (6 + Math.random() * 8) + "s";
+
+    particlesContainer.appendChild(particle);
+
+    setTimeout(() => {
+        particle.remove();
+    }, 15000);
+}
+
+setInterval(createParticle, 300);
