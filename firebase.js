bc195c53c9a32d4eb3e894bcbd8c07774b5150dc
Gideon Sleek <kapyengadeux27@gmail.com>
2025-12-12 02:44:41 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
-export const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
   authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
@@ -6,3 +9,9 @@ export const firebaseConfig = {
   messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MSG_SENDER,
   appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
 };
+
+// IMPORTANT: Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// IMPORTANT: Export Firestore
+export const db = getFirestore(app);
