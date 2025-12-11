8cbfaa768715a1f0c609913437b4950886c8505e
Gideon Sleek <kapyengadeux27@gmail.com>
2025-12-11 23:46:53 +0300

    create

---
+++ firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBUKZRg9NIwVG-68aiaxeCsIStC6UAtn4I",
+  authDomain: "sleek-tech-db.firebaseapp.com",
+  projectId: "sleek-tech-db",
+  storageBucket: "sleek-tech-db.firebasestorage.app",
+  messagingSenderId: "1084439747120",
+  appId: "1:1084439747120:web:5cc1c572e314ded2a92bf5"
+};
