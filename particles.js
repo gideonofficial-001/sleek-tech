57274f529e48c8b860438fc7c839e841c0fd385e
Gideon Sleek <kapyengadeux27@gmail.com>
2026-01-09 13:41:09 +0300

    Refactor particle creation for better readability

--- particles.js
+++ particles.js
@@ -1,17 +1,15 @@
 const particlesContainer = document.getElementById("particles");
 
 function createParticle() {
-    const particle = document.createElement("div");
-    particle.classList.add("particle");
+  const particle = document.createElement("div");
+  particle.classList.add("particle");
 
-    particle.style.left = Math.random() * 100 + "vw";
-    particle.style.animationDuration = (6 + Math.random() * 8) + "s";
+  particle.style.left = Math.random() * 100 + "vw";
+  particle.style.animationDuration = (6 + Math.random() * 8) + "s";
 
-    particlesContainer.appendChild(particle);
+  particlesContainer.appendChild(particle);
 
-    setTimeout(() => {
-        particle.remove();
-    }, 15000);
+  setTimeout(() => particle.remove(), 15000);
 }
 
 setInterval(createParticle, 300);
